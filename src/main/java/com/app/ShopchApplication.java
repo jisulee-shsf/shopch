5a9943cf81b4a1e34fa97efdaa1ff46ce8064807
jisulee-shsf <jisulee.shsf@gmail.com>
2025-02-20 04:06:24 +0900

    init: shopch 프로젝트 시작

---
+++ src/main/java/com/app/ShopchApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopchApplication.class, args);
+    }
+
+}
