f00951ba29540e0dc6a8e38b90d842c2fb24473b
jisulee-shsf <jisulee.shsf@gmail.com>
2025-02-26 18:12:26 +0900

    test: Health Check API 테스트 코드 작성

---
+++ src/test/java/com/app/api/health/controller/HealthCheckControllerTest.java
@@ -0,0 +1,31 @@
+package com.app.api.health.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthCheckController.class)
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Environment environment;
+
+    @DisplayName("헬스 체크를 한다.")
+    @Test
+    void healthCheck() throws Exception {
+        // given
+        // when & then
+        mockMvc.perform(get("/api/health")
+                )
+                .andExpect(status().isOk());
+    }
+}
