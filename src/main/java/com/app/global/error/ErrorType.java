92df28ca27c955e77f9b7f9cc3c3f0ba5f102609
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-07 18:47:46 +0900

    feat: AuthenticationException 생성 및 ErrorType 추가

--- src/main/java/com/app/global/error/ErrorType.java
+++ src/main/java/com/app/global/error/ErrorType.java
@@ -4,10 +4,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 @Getter
 @RequiredArgsConstructor
 public enum ErrorType {
-    ;
+
+    // 인증
+    EXPIRED_TOKEN(UNAUTHORIZED, "A-001", "해당 토큰은 만료되었습니다."),
+    INVALID_TOKEN(UNAUTHORIZED, "A-002", "해당 토큰은 유효하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
