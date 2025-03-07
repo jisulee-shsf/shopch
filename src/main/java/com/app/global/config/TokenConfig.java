0fbf09132682ceaffa60c5323650a2d8af92e723
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-07 18:50:02 +0900

    feat: TokenManager 빈 등록

---
+++ src/main/java/com/app/global/config/TokenConfig.java
@@ -0,0 +1,30 @@
+package com.app.global.config;
+
+import com.app.global.jwt.service.TokenManager;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+import static io.jsonwebtoken.io.Decoders.BASE64URL;
+
+@Configuration
+public class TokenConfig {
+
+    @Value("${token.access-token-expiration-duration}")
+    private String accessTokenExpirationDuration;
+
+    @Value("${token.refresh-token-expiration-duration}")
+    private String refreshTokenExpirationDuration;
+
+    @Value("${token.secret}")
+    private String tokenSecret;
+
+    @Bean
+    public TokenManager tokenManager() {
+        SecretKey secretKey = Keys.hmacShaKeyFor(BASE64URL.decode(tokenSecret));
+        return new TokenManager(accessTokenExpirationDuration, refreshTokenExpirationDuration, secretKey);
+    }
+}
