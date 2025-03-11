e777443673a5cef5e09bb3e35862dbe0a28bfc75
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-11 23:27:53 +0900

    feat: KakaoTokenController 생성

---
+++ src/main/java/com/app/web/controller/KakaoTokenController.java
@@ -0,0 +1,38 @@
+package com.app.web.controller;
+
+import com.app.web.client.KakaoTokenClient;
+import com.app.web.dto.KakaoTokenRequest;
+import com.app.web.dto.KakaoTokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KakaoTokenController {
+
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id}")
+    private String clientId;
+
+    @Value("${kakao.redirect.uri}")
+    private String redirectUri;
+
+    @Value("${kakao.client.secret}")
+    private String clientSecret;
+
+    @GetMapping("/oauth/kakao/callback")
+    public ResponseEntity<KakaoTokenResponse> loginCallback(String code) {
+        KakaoTokenRequest request = KakaoTokenRequest.builder()
+                .grant_type("authorization_code")
+                .client_id(clientId)
+                .redirect_uri(redirectUri)
+                .code(code)
+                .client_secret(clientSecret)
+                .build();
+        return ResponseEntity.ok(kakaoTokenClient.requestKakaoToken(request));
+    }
+}
