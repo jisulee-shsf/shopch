3eb2482d469cce09043dbeec47c236dc1522adeb
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-11 23:18:18 +0900

    feat: DTO(KakaoTokenRequest, KakaoTokenResponse) 생성

---
+++ src/main/java/com/app/web/dto/KakaoTokenRequest.java
@@ -0,0 +1,23 @@
+package com.app.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class KakaoTokenRequest {
+
+    private String grant_type;
+    private String client_id;
+    private String redirect_uri;
+    private String code;
+    private String client_secret;
+
+    @Builder
+    private KakaoTokenRequest(String grant_type, String client_id, String redirect_uri, String code, String client_secret) {
+        this.grant_type = grant_type;
+        this.client_id = client_id;
+        this.redirect_uri = redirect_uri;
+        this.code = code;
+        this.client_secret = client_secret;
+    }
+}
---
+++ src/main/java/com/app/web/dto/KakaoTokenResponse.java
@@ -0,0 +1,38 @@
+package com.app.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoTokenResponse {
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private Integer expiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("refresh_token_expires_in")
+    private Integer refreshTokenExpiresIn;
+
+    private String scope;
+
+    @Builder
+    private KakaoTokenResponse(String tokenType, String accessToken, Integer expiresIn, String refreshToken, Integer refreshTokenExpiresIn, String scope) {
+        this.tokenType = tokenType;
+        this.accessToken = accessToken;
+        this.expiresIn = expiresIn;
+        this.refreshToken = refreshToken;
+        this.refreshTokenExpiresIn = refreshTokenExpiresIn;
+        this.scope = scope;
+    }
+}
