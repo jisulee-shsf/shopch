90ac80e6afd18c96520cda047e6aeb45ab3fe9cf
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-12 18:34:52 +0900

    test: KakaoTokenController 테스트 코드 작성

---
+++ src/test/java/com/app/web/controller/KakaoTokenControllerTest.java
@@ -0,0 +1,57 @@
+package com.app.web.controller;
+
+import com.app.web.client.KakaoTokenClient;
+import com.app.web.dto.KakaoTokenRequest;
+import com.app.web.dto.KakaoTokenResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(KakaoTokenController.class)
+class KakaoTokenControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private KakaoTokenClient kakaoTokenClient;
+
+    @DisplayName("카카오 인가 코드로 토큰 발급을 요청한다.")
+    @Test
+    void loginCallback() throws Exception {
+        // given
+        KakaoTokenResponse response = KakaoTokenResponse.builder()
+                .tokenType("bearer")
+                .accessToken("access-token")
+                .expiresIn(21599)
+                .refreshToken("refresh-token")
+                .refreshTokenExpiresIn(5183999)
+                .scope("account-email profile-image profile-nickname")
+                .build();
+
+        given(kakaoTokenClient.requestKakaoToken(any(KakaoTokenRequest.class)))
+                .willReturn(response);
+
+        // when & then
+        mockMvc.perform(get("/oauth/kakao/callback")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token_type").value(response.getTokenType()))
+                .andExpect(jsonPath("$.access_token").value(response.getAccessToken()))
+                .andExpect(jsonPath("$.expires_in").value(response.getExpiresIn()))
+                .andExpect(jsonPath("$.refresh_token").value(response.getRefreshToken()))
+                .andExpect(jsonPath("$.refresh_token_expires_in").value(response.getRefreshTokenExpiresIn()))
+                .andExpect(jsonPath("$.scope").value(response.getScope()));
+    }
+}
