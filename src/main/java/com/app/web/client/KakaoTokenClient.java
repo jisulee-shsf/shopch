741e0c498641131dae65e7e0e59acd6af080e6fc
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-12 15:39:01 +0900

    refactor: SpringQueryMap 애노테이션 추가

--- src/main/java/com/app/web/client/KakaoTokenClient.java
+++ src/main/java/com/app/web/client/KakaoTokenClient.java
@@ -3,15 +3,14 @@ package com.app.web.client;
 import com.app.web.dto.KakaoTokenRequest;
 import com.app.web.dto.KakaoTokenResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import static org.springframework.http.MediaType.*;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
 @FeignClient(name = "kakaoTokenClient", url = "${kakao-token.feign.url}")
 public interface KakaoTokenClient {
 
     @PostMapping(value = "/oauth/token", consumes = APPLICATION_FORM_URLENCODED_VALUE)
-    KakaoTokenResponse requestKakaoToken(KakaoTokenRequest request);
+    KakaoTokenResponse requestKakaoToken(@SpringQueryMap KakaoTokenRequest request);
 }
