a693b029aa2d1378b4371c0cdbfb3b1329e37ae3
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-15 00:31:56 +0900

    feat: OauthLoginController 생성

---
+++ src/main/java/com/app/api/login/controller/OauthLoginController.java
@@ -0,0 +1,43 @@
+package com.app.api.login.controller;
+
+import com.app.api.login.dto.OauthLoginRequest;
+import com.app.api.login.dto.OauthLoginResponse;
+import com.app.api.login.service.OauthLoginService;
+import com.app.domain.member.constant.MemberType;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+import static com.app.api.login.validator.OauthValidator.validateAuthorizationHeader;
+import static com.app.api.login.validator.OauthValidator.validateMemberType;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class OauthLoginController {
+
+    private final OauthLoginService oauthLoginService;
+
+    @PostMapping("/oauth/login")
+    public ResponseEntity<OauthLoginResponse> oauthLogin(HttpServletRequest httpServletRequest,
+                                                         @RequestBody OauthLoginRequest oauthLoginRequest) {
+        String authorizationHeader = httpServletRequest.getHeader(AUTHORIZATION);
+        validateAuthorizationHeader(authorizationHeader);
+
+        String memberType = oauthLoginRequest.getMemberType();
+        validateMemberType(memberType);
+
+        String accessToken = authorizationHeader.split(" ")[1];
+        Date issueDate = new Date();
+        OauthLoginResponse response = oauthLoginService.oauthLogin(MemberType.from(memberType), accessToken, issueDate);
+
+        return ResponseEntity.ok(response);
+    }
+}
