56c6af47ddc32d68d8dffa258b757844020807da
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-15 00:28:05 +0900

    feat: 인증 및 회원 에러 타입 추가

--- src/main/java/com/app/global/error/ErrorType.java
+++ src/main/java/com/app/global/error/ErrorType.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @Getter
@@ -12,7 +13,13 @@ public enum ErrorType {
 
     // 인증
     EXPIRED_TOKEN(UNAUTHORIZED, "A-001", "해당 토큰은 만료되었습니다."),
-    INVALID_TOKEN(UNAUTHORIZED, "A-002", "해당 토큰은 유효하지 않습니다.");
+    INVALID_TOKEN(UNAUTHORIZED, "A-002", "해당 토큰은 유효하지 않습니다."),
+    MISSING_AUTHORIZATION_HEADER(UNAUTHORIZED, "A-003", "Authorization 헤더가 없습니다."),
+    INVALID_GRANT_TYPE(UNAUTHORIZED, "A-004", "Bearer 인증 타입이 아닙니다."),
+
+    // 회원
+    INVALID_MEMBER_TYPE(BAD_REQUEST, "M-001", "잘못된 회원 타입입니다."),
+    ALREADY_REGISTERED_MEMBER(BAD_REQUEST, "M-002", "이미 가입된 회원입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
