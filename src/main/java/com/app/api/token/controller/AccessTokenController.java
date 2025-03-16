ce78aa92c1003685326d9af11a025519478ccdd8
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-16 22:24:00 +0900

    feat: AccessTokenController 생성

---
+++ src/main/java/com/app/api/token/controller/AccessTokenController.java
@@ -0,0 +1,35 @@
+package com.app.api.token.controller;
+
+import com.app.api.token.dto.AccessTokenResponse;
+import com.app.api.token.service.AccessTokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+import static com.app.global.util.AuthorizationHeaderUtils.validateAuthorizationHeader;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AccessTokenController {
+
+    private final AccessTokenService accessTokenService;
+
+    @PostMapping("/access-token/issue")
+    public ResponseEntity<AccessTokenResponse> createAccessToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        validateAuthorizationHeader(authorizationHeader);
+
+        String refreshToken = authorizationHeader.split(" ")[1];
+        Date reissueDate = new Date();
+        AccessTokenResponse response = accessTokenService.createAccessTokenByRefreshToken(refreshToken, reissueDate);
+
+        return ResponseEntity.ok(response);
+    }
+}
