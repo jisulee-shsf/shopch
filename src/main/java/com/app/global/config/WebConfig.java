1965c238f6d5e9129d6d08f4ab579966b86efc9f
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-22 23:52:28 +0900

    chore: AuthenticationInterceptor 등록

--- src/main/java/com/app/global/config/WebConfig.java
+++ src/main/java/com/app/global/config/WebConfig.java
@@ -1,13 +1,19 @@
 package com.app.global.config;
 
+import com.app.global.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -20,6 +26,19 @@ public class WebConfig implements WebMvcConfigurer {
                         PATCH.name(),
                         DELETE.name(),
                         OPTIONS.name()
+                );
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/health",
+                        "/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout"
                 );
     }
 }
