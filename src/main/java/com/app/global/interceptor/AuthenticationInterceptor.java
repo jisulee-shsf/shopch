6f4c9e050d490240a1ca0f0386ba870ad09e8b07
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-22 23:50:26 +0900

    feat: AuthenticationInterceptor 구현

---
+++ src/main/java/com/app/global/interceptor/AuthenticationInterceptor.java
@@ -0,0 +1,38 @@
+package com.app.global.interceptor;
+
+import com.app.global.error.exception.AuthenticationException;
+import com.app.global.jwt.constant.TokenType;
+import com.app.global.jwt.service.TokenManager;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static com.app.global.error.ErrorType.INVALID_TOKEN_TYPE;
+import static com.app.global.util.AuthorizationHeaderUtils.validateAuthorizationHeader;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    private final TokenManager tokenManager;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        validateAuthorizationHeader(authorizationHeader);
+
+        String accessToken = authorizationHeader.split(" ")[1];
+        tokenManager.validateToken(accessToken);
+
+        Claims claims = tokenManager.getTokenClaims(accessToken);
+        String tokenType = claims.getSubject();
+        if (!TokenType.isAccessToken(tokenType)) {
+            throw new AuthenticationException(INVALID_TOKEN_TYPE);
+        }
+        return true;
+    }
+}
