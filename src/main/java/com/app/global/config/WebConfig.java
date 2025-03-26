c8b731788181b274007b39b2a016f3aa42f24f15
jisulee-shsf <jisulee.shsf@gmail.com>
2025-03-27 02:39:14 +0900

    chore: MemberInfoArgumentResolver 등록

--- src/main/java/com/app/global/config/WebConfig.java
+++ src/main/java/com/app/global/config/WebConfig.java
@@ -1,11 +1,15 @@
 package com.app.global.config;
 
 import com.app.global.interceptor.AuthenticationInterceptor;
+import com.app.global.resolver.MemberInfoArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 import static org.springframework.http.HttpMethod.*;
 
@@ -14,6 +18,7 @@ import static org.springframework.http.HttpMethod.*;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
+    private final MemberInfoArgumentResolver memberInfoArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -40,5 +45,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/access-token/issue",
                         "/api/logout"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberInfoArgumentResolver);
     }
 }
