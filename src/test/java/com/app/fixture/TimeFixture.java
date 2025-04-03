e659993354984b328c2f12f5fc524d343b5f3f9f
jisulee-shsf <jisulee.shsf@gmail.com>
2025-04-03 23:49:58 +0900

    test: TimeFixture 생성

--- src/test/java/com/app/fixture/TimeFixture.java
+++ src/test/java/com/app/fixture/TimeFixture.java
@@ -4,8 +4,7 @@ import java.time.Instant;
 
 public class TimeFixture {
 
-    public static final Instant FIXED_FUTURE_INSTANT = Instant.parse("2025-12-31T01:00:00Z");
     public static final Instant FIXED_PAST_INSTANT = Instant.parse("2025-01-01T01:00:00Z");
-    public static final Long ACCESS_TOKEN_EXPIRATION_DURATION = 900000L;
-    public static final Long REFRESH_TOKEN_EXPIRATION_DURATION = 1209600000L;
+    public static final long ACCESS_TOKEN_EXPIRATION_DURATION = 1000 * 60 * 15 * 15;
+    public static final long REFRESH_TOKEN_EXPIRATION_DURATION = 1000 * 60 * 60 * 24 * 14;
 }
