2f1a7d499f259fa55d2d411539d830addce20dad
jisulee-shsf <jisulee.shsf@gmail.com>
2025-04-04 14:47:54 +0900

    fix: TimeFixture의 액세스 토큰 만료 기간 계산 수정

--- src/test/java/com/app/fixture/TimeFixture.java
+++ src/test/java/com/app/fixture/TimeFixture.java
@@ -5,6 +5,6 @@ import java.time.Instant;
 public class TimeFixture {
 
     public static final Instant FIXED_PAST_INSTANT = Instant.parse("2025-01-01T01:00:00Z");
-    public static final long ACCESS_TOKEN_EXPIRATION_DURATION = 1000 * 60 * 15 * 15;
+    public static final long ACCESS_TOKEN_EXPIRATION_DURATION = 1000 * 60 * 15;
     public static final long REFRESH_TOKEN_EXPIRATION_DURATION = 1000 * 60 * 60 * 24 * 14;
 }
