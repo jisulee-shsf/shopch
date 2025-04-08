d198c1fc035ff074ec8890bee7d9763a1e07b95f
jisulee-shsf <jisulee.shsf@gmail.com>
2025-04-09 00:10:36 +0900

    refactor: RestDocsSupport 설정 변경

--- src/test/java/com/app/support/RestDocsSupport.java
+++ src/test/java/com/app/support/RestDocsSupport.java
@@ -1,5 +1,6 @@
 package com.app.support;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -8,27 +9,32 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsSupport {
 
     protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setUp(RestDocumentationContextProvider provider) {
         mockMvc = standaloneSetup(initController())
                 .apply(documentationConfiguration(provider)
-                        .uris()
-                        .withScheme("https")
-                        .withHost("jisulee-shsf.com")
-                        .withPort(443)
-                        .and()
                         .operationPreprocessors()
-                        .withRequestDefaults(prettyPrint(), modifyHeaders().remove(CONTENT_LENGTH))
-                        .withResponseDefaults(prettyPrint(), modifyHeaders().remove(CONTENT_LENGTH))
+                        .withRequestDefaults(
+                                modifyUris()
+                                        .scheme("https")
+                                        .host("jisulee-shsf.com")
+                                        .removePort(),
+                                prettyPrint(),
+                                modifyHeaders().remove(CONTENT_LENGTH)
+                        )
+                        .withResponseDefaults(
+                                prettyPrint(),
+                                modifyHeaders().remove(CONTENT_LENGTH)
+                        )
                 )
                 .build();
     }
