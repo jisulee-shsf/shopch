28950ba387cf1c8d0f4070a7d4294e6e750a9625
jisulee-shsf <jisulee.shsf@gmail.com>
2025-04-13 10:17:08 +0900

    fix: 응답 DTO의 LocalDateTime 필드 JsonFormat에 timezone 속성 추가

--- src/main/java/com/app/api/token/dto/AccessTokenResponse.java
+++ src/main/java/com/app/api/token/dto/AccessTokenResponse.java
@@ -6,14 +6,12 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
-
 @Getter
 public class AccessTokenResponse {
 
     private String grantType;
     private String accessToken;
-    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime accessTokenExpirationDateTime;
 
     @Builder
