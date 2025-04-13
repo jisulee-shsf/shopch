b85c85b68e8fadea92f10dd0cfbc2afd0764c787
jisulee-shsf <jisulee.shsf@gmail.com>
2025-04-13 10:24:17 +0900

    refactor: 테스트 코드의 ZoneId를 UTC로 설정

--- src/test/java/com/app/fixture/TimeFixture.java
+++ src/test/java/com/app/fixture/TimeFixture.java
@@ -2,11 +2,12 @@ package com.app.fixture;
 
 import java.time.Clock;
 import java.time.Instant;
-
-import static java.time.ZoneId.systemDefault;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public class TimeFixture {
 
     public static final Instant FIXED_INSTANT = Instant.parse("2025-01-01T01:00:00Z");
-    public static final Clock FIXED_CLOCK = Clock.fixed(FIXED_INSTANT, systemDefault());
+    public static final ZoneId FIXED_TIME_ZONE = ZoneOffset.UTC;
+    public static final Clock FIXED_CLOCK = Clock.fixed(FIXED_INSTANT, FIXED_TIME_ZONE);
 }
