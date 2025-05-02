143a9d792c36dc308e75397f16d4889780b9b88c
jisulee-shsf <jisulee.shsf@gmail.com>
2025-05-03 00:36:57 +0900

    test: Product 테스트 코드 추가

--- src/test/java/com/app/domain/product/entity/ProductTest.java
+++ src/test/java/com/app/domain/product/entity/ProductTest.java
@@ -1,6 +1,8 @@
 package com.app.domain.product.entity;
 
 import com.app.api.product.dto.request.ProductUpdateRequest;
+import com.app.global.error.ErrorType;
+import com.app.global.error.exception.OutOfStockException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,7 @@ import static com.app.domain.product.constant.ProductSellingStatus.SELLING;
 import static com.app.domain.product.constant.ProductType.PRODUCT_A;
 import static com.app.domain.product.constant.ProductType.PRODUCT_B;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ProductTest {
 
@@ -76,5 +79,53 @@ class ProductTest {
         // then
         assertThat(product.getStockQuantity()).isZero();
         assertThat(product.getProductSellingStatus()).isEqualTo(COMPLETED);
+    }
+
+    @DisplayName("상품 재고를 주문 수량만큼 차감한다.")
+    @Test
+    void deductStockQuantity() {
+        // given
+        Product product = createTestProduct(2);
+
+        // when
+        product.deductStockQuantity(1);
+
+        // then
+        assertThat(product.getStockQuantity()).isEqualTo(1);
+    }
+
+    @DisplayName("상품 재고가 주문 수량보다 적을 때 차감을 시도할 경우, 예외가 발생한다.")
+    @Test
+    void deductStockQuantity_StockQuantityLessThanOrderQuantity() {
+        // given
+        Product product = createTestProduct(1);
+
+        // when & then
+        assertThatThrownBy(() -> product.deductStockQuantity(2))
+                .isInstanceOf(OutOfStockException.class)
+                .hasMessage(ErrorType.OUT_OF_STOCK.getErrorMessage());
+    }
+
+    @DisplayName("차감한 상품 재고가 0일 경우, 판매 상태를 SELLING에서 COMPLETED로 변경한다.")
+    @Test
+    void deductStockQuantity_ZeroStockQuantity() {
+        // given
+        Product product = createTestProduct(1);
+
+        // when
+        product.deductStockQuantity(1);
+
+        // then
+        assertThat(product.getProductSellingStatus()).isEqualTo(COMPLETED);
+    }
+
+    private Product createTestProduct(int stockQuantity) {
+        return Product.builder()
+                .name("product")
+                .productType(PRODUCT_A)
+                .productSellingStatus(SELLING)
+                .price(10000)
+                .stockQuantity(stockQuantity)
+                .build();
     }
 }
