ae147e9f8468dc5abdb92bc97c8b5165a5ed038f
jisulee-shsf <jisulee.shsf@gmail.com>
2025-05-13 13:54:15 +0900

    refactor: 상품과 주문 요청 DTO의 필드 타입 변경(int -> Integer) 및 검증 조건 추가

--- src/main/java/com/app/api/product/dto/request/ProductCreateRequest.java
+++ src/main/java/com/app/api/product/dto/request/ProductCreateRequest.java
@@ -4,6 +4,7 @@ import com.app.domain.product.constant.ProductType;
 import com.app.domain.product.entity.Product;
 import com.app.global.validator.ValidEnum;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,14 +21,16 @@ public class ProductCreateRequest {
     @ValidEnum(enumClass = ProductType.class, message = "유효한 상품 타입이 아닙니다.")
     private String productType;
 
+    @NotNull(message = "등록 상품 가격은 필수입니다.")
     @Positive(message = "등록 상품 가격은 양수여야 합니다.")
-    private int price;
+    private Integer price;
 
+    @NotNull(message = "등록 상품 재고 수량은 필수입니다.")
     @Positive(message = "등록 상품 재고 수량은 양수여야 합니다.")
-    private int stockQuantity;
+    private Integer stockQuantity;
 
     @Builder
-    private ProductCreateRequest(String name, String productType, int price, int stockQuantity) {
+    private ProductCreateRequest(String name, String productType, Integer price, Integer stockQuantity) {
         this.name = name;
         this.productType = productType;
         this.price = price;
