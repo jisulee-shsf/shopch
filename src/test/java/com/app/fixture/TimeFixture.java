f45682704bc8846fdd1604aefc58e27567439621
jisulee-shsf <jisulee.shsf@gmail.com>
2025-06-24 22:21:06 +0900

    test: TimeFixture의 FIXED_CLOCK 제거

--- src/test/java/com/app/fixture/TimeFixture.java
+++ src/test/java/com/app/fixture/TimeFixture.java
@@ -1,6 +1,5 @@
 package com.app.fixture;
 
-import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -9,5 +8,4 @@ public class TimeFixture {
 
     public static final Instant FIXED_INSTANT = Instant.parse("2025-01-01T01:00:00Z");
     public static final ZoneId FIXED_TIME_ZONE = ZoneOffset.UTC;
-    public static final Clock FIXED_CLOCK = Clock.fixed(FIXED_INSTANT, FIXED_TIME_ZONE);
 }
