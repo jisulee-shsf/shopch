7c4fab36455c8101c2c3534f2a057545aca40844
jisulee-shsf <jisulee.shsf@gmail.com>
2025-08-15 17:57:44 +0900

    refactor: 액세스 토큰 갱신의 리프레시 토큰 로직을 Member에서 RefreshToken 기반으로 변경

--- src/main/java/com/app/api/token/service/TokenService.java
+++ src/main/java/com/app/api/token/service/TokenService.java
@@ -2,7 +2,8 @@ package com.app.api.token.service;
 
 import com.app.api.token.service.dto.response.AccessTokenResponse;
 import com.app.domain.member.entity.Member;
-import com.app.domain.member.service.MemberService;
+import com.app.domain.token.entity.RefreshToken;
+import com.app.domain.token.service.RefreshTokenService;
 import com.app.global.jwt.constant.AuthenticationScheme;
 import com.app.global.jwt.service.TokenManager;
 import lombok.RequiredArgsConstructor;
@@ -13,17 +14,19 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class TokenService {
 
-    private final MemberService memberService;
+    private final RefreshTokenService refreshTokenService;
     private final TokenManager tokenManager;
 
     public AccessTokenResponse refreshAccessToken(String refreshToken, Date issueDate) {
         tokenManager.validateRefreshToken(refreshToken);
 
-        Member member = memberService.getMemberByRefreshToken(refreshToken);
+        RefreshToken refreshTokenEntity = refreshTokenService.getRefreshTokenByToken(refreshToken);
+        Member member = refreshTokenEntity.getMember();
+
         String accessToken = tokenManager.createAccessToken(member.getId(), member.getRole(), issueDate);
         LocalDateTime accessTokenExpirationDateTime = tokenManager.extractExpirationDateTime(accessToken);
 
