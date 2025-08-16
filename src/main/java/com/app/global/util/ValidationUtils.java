f65cd067e1ca4932904acaefe9a077cebfc5d904
jisulee-shsf <jisulee.shsf@gmail.com>
2025-08-16 19:16:03 +0900

    feat: ValidationUtils 구현

---
+++ src/main/java/com/app/global/util/ValidationUtils.java
@@ -0,0 +1,24 @@
+package com.app.global.util;
+
+import com.app.global.error.ErrorType;
+import com.app.global.error.exception.IllegalArgumentException;
+import org.springframework.util.StringUtils;
+
+public class ValidationUtils {
+
+    public static void notNull(Object object, ErrorType errorType) {
+        if (object == null) {
+            throw new IllegalArgumentException(errorType);
+        }
+    }
+
+    public static void hasText(String text, ErrorType errorType) {
+        if (hasNoText(text)) {
+            throw new IllegalArgumentException(errorType);
+        }
+    }
+
+    public static boolean hasNoText(String text) {
+        return !StringUtils.hasText(text);
+    }
+}
