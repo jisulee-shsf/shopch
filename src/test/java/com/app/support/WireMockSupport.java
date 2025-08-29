bf0ebd1355c6489682d73e1fc27942fb076101e5
jisulee-shsf <jisulee.shsf@gmail.com>
2025-08-29 11:45:24 +0900

    test: WireMock 테스트 설정 및 응답 JSON 변경

--- src/test/java/com/app/support/WireMockSupport.java
+++ src/test/java/com/app/support/WireMockSupport.java
@@ -1,34 +1,22 @@
 package com.app.support;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureWireMock(port = 0)
-@TestPropertySource(
-        properties = {
-                "kakao-token.feign.url=http://localhost:${wiremock.server.port}",
-                "kakao-user-info.feign.url=http://localhost:${wiremock.server.port}"
-        }
-)
+@ActiveProfiles("test")
 public abstract class WireMockSupport {
 
     @Autowired
-    private WireMockServer wireMockServer;
+    protected WireMockServer wireMockServer;
 
     @BeforeEach
-    void beforeEach() {
-        wireMockServer.stop();
-        wireMockServer.start();
-    }
-
-    @AfterEach
-    void afterEach() {
+    void setUp() {
         wireMockServer.resetAll();
     }
 }
