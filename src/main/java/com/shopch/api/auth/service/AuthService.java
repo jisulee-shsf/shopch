9cb0fec5066233302f1799e6d7d301c8a6e86f4e
jisulee-shsf <jisulee.shsf@gmail.com>
2025-09-01 13:50:06 +0900

    refactor: 로그인, 액세스 토큰 갱신, 로그아웃 서비스 클래스 통합

--- src/main/java/com/shopch/api/auth/service/AuthService.java
+++ src/main/java/com/shopch/api/auth/service/AuthService.java
@@ -1,17 +1,20 @@
 package com.shopch.api.auth.service;
 
 import com.shopch.api.auth.service.dto.request.LoginServiceRequest;
+import com.shopch.api.auth.service.dto.request.RefreshAccessTokenServiceRequest;
+import com.shopch.api.auth.service.dto.response.AccessTokenResponse;
 import com.shopch.api.auth.service.dto.response.LoginResponse;
-import com.shopch.external.oauth.constant.OAuthProvider;
 import com.shopch.domain.member.constant.Role;
 import com.shopch.domain.member.entity.Member;
 import com.shopch.domain.member.service.MemberService;
+import com.shopch.domain.token.entity.RefreshToken;
 import com.shopch.domain.token.service.RefreshTokenService;
+import com.shopch.external.oauth.constant.OAuthProvider;
 import com.shopch.external.oauth.dto.UserInfo;
 import com.shopch.external.oauth.service.SocialLoginService;
 import com.shopch.external.oauth.service.SocialLoginServiceFactory;
-import com.shopch.global.jwt.dto.TokenPair;
 import com.shopch.global.jwt.JwtTokenProvider;
+import com.shopch.global.jwt.dto.TokenPair;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,23 +23,42 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
-public class LoginService {
+@Transactional(readOnly = true)
+public class AuthService {
 
     private final MemberService memberService;
     private final RefreshTokenService refreshTokenService;
     private final SocialLoginServiceFactory socialLoginServiceFactory;
-    private final JwtTokenProvider jwtProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    @Transactional
     public LoginResponse login(LoginServiceRequest request, Date issuedAt) {
         UserInfo userInfo = getUserInfoFromSocialLoginService(request.getOauthProvider(), request.getCode());
         Member member = getOrRegisterMember(userInfo);
 
-        TokenPair tokenPair = jwtProvider.createTokenPair(member, issuedAt);
+        TokenPair tokenPair = jwtTokenProvider.createTokenPair(member, issuedAt);
         updateOrRegisterRefreshToken(member, tokenPair);
 
         return LoginResponse.of(tokenPair);
+    }
+
+    public AccessTokenResponse refreshAccessToken(RefreshAccessTokenServiceRequest request, Date issuedAt) {
+        String refreshToken = request.getRefreshToken();
+        jwtTokenProvider.validateRefreshToken(refreshToken);
+
+        RefreshToken refreshTokenEntity = refreshTokenService.getRefreshToken(refreshToken);
+        Member member = refreshTokenEntity.getMember();
+
+        String accessToken = jwtTokenProvider.createAccessToken(member, issuedAt);
+        LocalDateTime accessTokenExpiresAt = jwtTokenProvider.getExpirationFrom(accessToken);
+
+        return AccessTokenResponse.of(accessToken, accessTokenExpiresAt);
+    }
+
+    @Transactional
+    public void logout(Long memberId) {
+        refreshTokenService.deleteRefreshToken(memberId);
     }
 
     private UserInfo getUserInfoFromSocialLoginService(OAuthProvider oauthProvider, String code) {
@@ -54,12 +76,5 @@ public class LoginService {
                 .ifPresentOrElse(
                         refreshToken -> refreshToken.updateToken(tokenPair.getRefreshToken(), tokenPair.getRefreshTokenExpiresAt()),
                         () -> refreshTokenService.registerRefreshToken(tokenPair.toRefreshToken(member)));
-    }
-
-    public void deleteAccount(Long memberId, LocalDateTime deletedAt) {
-        refreshTokenService.deleteRefreshToken(memberId);
-
-        Member member = memberService.getMember(memberId);
-        member.updateDeletedAt(deletedAt);
     }
 }
