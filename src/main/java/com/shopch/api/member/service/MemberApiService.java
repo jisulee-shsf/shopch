c9286fdc1c294a7beb40bc87d2ee9f05bcff1fab
jisulee-shsf <jisulee.shsf@gmail.com>
2025-09-01 14:01:58 +0900

    refactor: 회원 조회 및 탈퇴 서비스 클래스 통합

--- src/main/java/com/shopch/api/member/service/MemberApiService.java
+++ src/main/java/com/shopch/api/member/service/MemberApiService.java
@@ -1,19 +1,32 @@
 package com.shopch.api.member.service;
 
 import com.shopch.api.member.service.dto.MemberInfoResponse;
+import com.shopch.domain.member.entity.Member;
 import com.shopch.domain.member.service.MemberService;
+import com.shopch.domain.token.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class MemberInfoService {
+@Transactional(readOnly = true)
+public class MemberApiService {
 
     private final MemberService memberService;
+    private final RefreshTokenService refreshTokenService;
 
     public MemberInfoResponse getMemberInfo(Long memberId) {
         return MemberInfoResponse.of(memberService.getMember(memberId));
+    }
+
+    @Transactional
+    public void deleteMember(Long memberId, LocalDateTime deletedAt) {
+        refreshTokenService.deleteRefreshToken(memberId);
+
+        Member member = memberService.getMember(memberId);
+        member.updateDeletedAt(deletedAt);
     }
 }
